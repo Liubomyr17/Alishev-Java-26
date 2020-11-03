a931d50dcfd51232154ea0a5b58a5094b822e60a
Liubomyr Izdepskyi <41821541+Liubomyr17@users.noreply.github.com>
2020-11-03 22:35:26 +0200

    Add files via upload

---
+++ src/com/company/Solution.java
@@ -0,0 +1,13 @@
+package com.company;
+
+
+public class Solution {
+    public static void main(String[] args) {
+      Dog dog = new Dog();
+      dog.eat();
+      dog.showName();
+
+
+    }
+    }
+
